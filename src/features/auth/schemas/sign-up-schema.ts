2689d1e6d42c112d2b62eadbde6003b73578e5a7
gui <guisarria@gmail.com>
2024-10-26 20:35:37 -0300

    chore: user validation

--- src/features/auth/schemas/sign-up-schema.ts
+++ src/features/auth/schemas/sign-up-schema.ts
@@ -2,8 +2,7 @@ import * as z from "zod";
 
 export const SignUpSchema = z.object({
   email: z.string().email(),
-  name: z
-    .string()
+  name: z.string()
     .min(4, { message: "Please add a name with at least 4 characters" }),
   password: z.string().min(8, {
     message: "Password must be at least 8 characters long",
